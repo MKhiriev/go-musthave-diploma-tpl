21fb7d748bc731db205d2db1ebc7b3149b3a3e1d
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-10-22 11:57:48 +0500

    withdrawal: added missing order_id field

--- models/withdrawal.go
+++ models/withdrawal.go
@@ -5,6 +5,7 @@ import "time"
 type Withdrawal struct {
 	WithdrawalId int64     `gorm:"column:withdrawal_id" json:"-"`
 	UserId       int64     `gorm:"column:login" json:"-"`
+	OrderId      int64     `gorm:"column:order_id" json:"-"`
 	Sum          float64   `gorm:"column:sum" json:"sum"`
 	ProcessedAt  time.Time `gorm:"column:processed_at" json:"processed_at"`
 }

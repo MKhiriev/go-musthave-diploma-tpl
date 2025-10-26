1fa8453d3ed8de3bdace32e5d50bc5d4a228c095
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-10-26 15:57:51 +0500

    implemented FindBalanceByUserId method

--- internal/store/repository_balance.go
+++ internal/store/repository_balance.go
@@ -2,9 +2,13 @@ package store
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"go-musthave-diploma-tpl/internal/logger"
 	"go-musthave-diploma-tpl/models"
 
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
 	"gorm.io/gorm"
 )
 
@@ -21,6 +25,22 @@ func NewBalanceRepository(db *gorm.DB, logger *logger.Logger) BalanceRepository 
 }
 
 func (b *balanceRepository) FindBalanceByUserId(ctx context.Context, userId int64) (models.Balance, error) {
-	//TODO implement me
-	panic("implement me")
+	var balance models.Balance
+
+	err := b.db.WithContext(ctx).
+		Where("user_id = ?", userId).
+		Find(&balance).Error
+
+	var pgErr *pgconn.PgError
+	// if postgres returns error
+	if errors.As(err, &pgErr) {
+		switch pgErr.Code {
+		case pgerrcode.NoDataFound:
+			return models.Balance{}, ErrNoBalanceFound
+		default:
+			return models.Balance{}, fmt.Errorf("unexpected DB error: %w", err)
+		}
+	}
+
+	return balance, nil
 }
